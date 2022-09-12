d2f853e4beb5497dae130c838b4e8b88dd445c2d
Adam James <adamjames411@gmail.com>
2022-09-12 13:35:33 +0100

    fixed get links

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -25,7 +25,7 @@ const Index = ({ orders, products }) => {
     const currentStatus = item.status;
 
     try {
-      const res = await axios.put("http://localhost:3000/api/orders/" + id, {
+      const res = await axios.put("https://delishhhapp.netlify.app/api/orders/" + id, {
         status: currentStatus + 1,
       });
       setOrderList([
@@ -129,8 +129,8 @@ export const getServerSideProps = async (ctx) => {
     };
   }
 
-  const productRes = await axios.get("/api/products");
-  const orderRes = await axios.get("/api/orders");
+  const productRes = await axios.get("https://delishhhapp.netlify.app/api/products");
+  const orderRes = await axios.get("https://delishhhapp.netlify.app/api/orders");
 
   return {
     props: {
